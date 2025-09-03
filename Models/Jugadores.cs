6a3bc768ea7bbf8e9f8fbe6b375d3519541c556f
RomelOrtega <romel_ortega@ucne.edu.do>
2025-09-02 22:40:13 -0400

    Creado get y set de jugadores

---
+++ Models/Jugadores.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RegistroJugadores.Models
+{
+    public class Jugadores
+    {
+        [Key]
+        public int JugadorId { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
+
+        public string Nombre { get; set; } = null!;
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public double Partidas { get; set; }
+    }
+}
