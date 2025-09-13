0de2eb85ab498b14d53651027fff53ae7f575d4e
RomelOrtega <romel_ortega@ucne.edu.do>
2025-09-12 23:43:11 -0400

    Se añadio el onModelCreating por problemas con la BD

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -9,5 +9,34 @@ namespace RegistroJugadores.DAL
         public Contexto(DbContextOptions<Contexto> options) : base(options) { }
         public DbSet<Jugadores> Jugadores { get; set; } = null!;
         public DbSet<Partidas> Partidas { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Partidas>()
+                .HasOne(p => p.Jugador1)
+                .WithMany()
+                .HasForeignKey(p => p.Jugador1Id)
+                .OnDelete(DeleteBehavior.Restrict); 
+
+            modelBuilder.Entity<Partidas>()
+                .HasOne(p => p.Jugador2)
+                .WithMany()
+                .HasForeignKey(p => p.Jugador2Id)
+                .OnDelete(DeleteBehavior.Restrict); 
+
+            modelBuilder.Entity<Partidas>()
+                .HasOne(p => p.Ganador)
+                .WithMany()
+                .HasForeignKey(p => p.GanadorId)
+                .OnDelete(DeleteBehavior.Restrict); 
+
+            modelBuilder.Entity<Partidas>()
+                .HasOne(p => p.TurnoJugador)
+                .WithMany()
+                .HasForeignKey(p => p.TurnoJugadorId)
+                .OnDelete(DeleteBehavior.Restrict); 
+        }
     }
 }
