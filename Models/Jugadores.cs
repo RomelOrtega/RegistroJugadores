54bec076c079cfc4f5e5a888f9a7bc5c4bb3ac5a
RomelOrtega <romel_ortega@ucne.edu.do>
2025-09-19 20:08:06 -0400

    Push

--- Models/Jugadores.cs
+++ Models/Jugadores.cs
@@ -7,18 +7,15 @@ namespace RegistroJugadores.Models
     {
         [Key]
         public int JugadorId { get; set; }
-
         [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "No se permiten caracteres especiales")]
         [Required(ErrorMessage = "Este campo es requerido")]
         [MaxLength(100)]
         public string Nombre { get; set; } = null!;
-
         [Required(ErrorMessage = "Por favor digitar la cantidad de Victorias.")]
         [Range(0, double.MaxValue, ErrorMessage = "No puede ser negativo")]
         public double Victorias { get; set; } = 0;
         public int Derrotas { get; set; } = 0;
         public int Empates { get; set; } = 0;
-
         [InverseProperty(nameof(Models.Movimientos.Jugador))]
         public virtual ICollection<Movimientos> Movimientos { get; set; } = new List<Movimientos>();
     }
